0c10f59818a560a35e4676a5d0a7bb463c464e20
MurariuF <murariu.flavius@gmail.com>
2023-01-20 10:58:51 -0800

    first shot

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game bowlingGame = new Game();
+
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(3);
+            bowlingGame.Roll(2);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(3);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(6);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(7);
+            bowlingGame.Roll(8);
+            bowlingGame.Roll(8);
+            bowlingGame.Roll(9);
+            bowlingGame.Roll(9);
+            bowlingGame.Roll(9);
+            bowlingGame.Roll(9);
+            bowlingGame.Roll(9);
+            bowlingGame.Roll(9);
+            bowlingGame.Roll(10);
+            bowlingGame.Roll(9);
+            //bowlingGame.Roll(9);
+
+            Console.Write(bowlingGame.Score().ToString());
         }
     }
 }
