7391db8ea99ea7f7bfb002d35ba42bee118af0f5
MurariuF <murariu.flavius@gmail.com>
2023-01-22 17:54:03 +0200

    refactor all game

--- Game.cs
+++ Game.cs
@@ -1,68 +1,98 @@
-﻿namespace Bowling
+﻿using System.Runtime.CompilerServices;
+
+namespace Bowling
 {
     public class Game
     {
-        private int[] rolls = new int[21];
-        private int currentRoll;
+        List<Frame> frames = new List<Frame>();
+        static int NUM_OF_FRAMES = 10;
 
-        public void Roll(int pins)
+
+        public void Play()
         {
-            rolls[currentRoll++] = pins;
+            var random = new Random();
+            for(int index = 0; index < NUM_OF_FRAMES; index++) 
+            {
+                var maximumRolls = 2;
+                var frame = new Frame();
+                var previewsPinsDown = 0;
+                while(frame.GetNumberOfRolls() < maximumRolls)
+                {
+                    var pinsDown = GetRandomRollValue(index, frame.IsStrike, previewsPinsDown);
+                    Console.WriteLine($"{index} - {pinsDown}");
+                    previewsPinsDown = pinsDown;
+                    frame.Roll(pinsDown);
+                    AddBonuses(index, frame);
+                    if(frame.IsStrike && index != 9)
+                    {
+                        break;
+                    }
+                    if(maximumRolls == 2 && frame.IsStrike && index.Equals(9))
+                    {
+                        maximumRolls++;
+                    }
+                }
+                frames.Add(frame);
+            }
         }
 
-        public int Score()
+        public void AddBonuses(int lastIndex, Frame lastFrame)
         {
-            int score = 0;
-            int roll = 0;
-
-            for(int frame = 0; frame < 10; frame++) 
+            for(int i = lastIndex - 2; i < lastIndex; i++)
             {
-                Console.WriteLine($"Round {frame}: ");
-                if(IsStrike(roll))
+                if(i >= 0)
                 {
-                    score += 10 + StrikeBonus(roll);
-                    roll++;
-                }
-                else if(IsSpare(roll))
-                {
-                    score += 10 + SpareBonus(roll);
-                    roll += 2;
-                }
-                else
-                {
-                    score += SumOfBallsInFrame(roll);
-                    roll += 2;
-                    //rolls[21] = 0;
+                    if (frames[i].IsStrike && frames[i].BonusesNumber < 2)
+                    {
+                        frames[i].AddBonus(lastFrame.GetLastRoll());
+                    }
+                    else if (frames[i].IsSpare && frames[i].BonusesNumber < 1)
+                    {
+                        frames[i].AddBonus(lastFrame.GetLastRoll());
+                    }
                 }
             }
-
-            return score;
         }
 
-        private bool IsStrike(int roll)
+        public void ShowInfo()
         {
-            return rolls[roll] == 10;
+            Console.WriteLine("Runda    Strike     Spare    Score    Lovituri    Prima     A doua");
+        }
+        public void ShowScore()
+        {
+            this.ShowInfo();
+            for(int index = 0; index < frames.Count; index++)
+            {
+                Console.WriteLine($"{index + 1} - {frames[index].IsStrike} - {frames[index].IsSpare} - {frames[index].TotalScore} - " +
+                    $"{frames[index].GetNumberOfRolls()} - {frames[index].GetAllRolls()}");
+            }
         }
 
-        private bool IsSpare(int roll)
+        public int GetRandomRollValue(int frameIndex, bool isStrike, int previewsPinsDown)
         {
-            return rolls[roll] + rolls[roll + 1] == 10; 
+            var random = new Random();
+            if(isStrike && frameIndex == NUM_OF_FRAMES - 1 && previewsPinsDown == 10)
+            {
+                return random.Next(11);
+            }
+            else
+            {
+                //if(frameIndex == 9 && isStrike == false)
+                //{
+                //    return 10;
+                //}
+                return random.Next(11 - previewsPinsDown);
+
+            }
         }
 
-        private int SumOfBallsInFrame(int roll)
-        {
-            return rolls[roll] + rolls[roll + 1];
-        }
+        
 
-        private int SpareBonus(int roll)
-        {
-            return rolls[roll + 2];
-        }
+        //interfata in doua parti(daca sunt prea multe if-uri)
+        //runda obisnuita si ultima runda
+        //list cu iframe in joc si interfata cu doua metode prima care face handleFrame(int nr) si a doua care este isFrameOver()
 
-        private int StrikeBonus(int roll)
-        {
-            return rolls[roll + 1] + rolls[roll + 2];
-        }
+
 
 
     }
--- Program.cs
+++ Program.cs
@@ -6,29 +6,8 @@
         {
             Game bowlingGame = new Game();
 
-            bowlingGame.Roll(1);
-            bowlingGame.Roll(3);
-            bowlingGame.Roll(2);
-            bowlingGame.Roll(4);
-            bowlingGame.Roll(3);
-            bowlingGame.Roll(5);
-            bowlingGame.Roll(4);
-            bowlingGame.Roll(6);
-            bowlingGame.Roll(5);
-            bowlingGame.Roll(7);
-            bowlingGame.Roll(8);
-            bowlingGame.Roll(8);
-            bowlingGame.Roll(9);
-            bowlingGame.Roll(9);
-            bowlingGame.Roll(9);
-            bowlingGame.Roll(9);
-            bowlingGame.Roll(9);
-            bowlingGame.Roll(9);
-            bowlingGame.Roll(10);
-            bowlingGame.Roll(9);
-            //bowlingGame.Roll(9);
-
-            Console.Write(bowlingGame.Score().ToString());
+            bowlingGame.Play();
+            bowlingGame.ShowScore();
         }
     }
 }
